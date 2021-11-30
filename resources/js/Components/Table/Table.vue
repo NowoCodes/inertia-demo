012828f033a7c8f514557b55ee75971335f96b43
Favour <toluwaniokunowo@gmail.com>
2021-11-30 02:54:41 +0100

    add table components

---
+++ resources/js/Components/Table/Table.vue
@@ -0,0 +1,19 @@
+<template>
+    <table class="w-full whitespace-no-wrap">
+        <thead>
+            <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase bg-gray-50 border-b">
+                <slot name="head" />
+            </tr>
+        </thead>
+
+        <tbody>
+            <slot name="body" />
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    name: "Table"
+}
+</script>
